4f55d4d6df406367d0277f31cf7bf86d58f88b07
dave <michael828inoc@gmail.com>
2023-03-06 02:06:48 +0800

    first commit

---
+++ db.sql
@@ -0,0 +1,21 @@
+SELECT country_of_birth, count(*) FROM person GROUP BY country_of_birth;
+
+SELECT country_of_birth, count(1) FROM person 
+    GROUP BY country_of_birth HAVING COUNT(1) > 5 ORDER BY country_of_birth;
+
+SELECT * from car GROUP BY model;
+
+SELECT reltuples::bigint AS EstimatedCount
+FROM   pg_class
+WHERE  oid = 'public.car'::regclass;
+
+SELECT id, make, model, price, ROUND(price * .10, 2), ROUND(price - (price * .10), 2) FROM car
+    GROUP BY id, make, model, price;
+
+SELECT id, make, model, price AS original_price, 
+    ROUND(price * .10, 2) AS ten_percent,  
+    ROUND(price - (price * .10), 2) AS discount_after_10_percent 
+    FROM car GROUP BY id, make, model, price;
+
+SELECT COALESCE(email, 'email not provided') FROM person;
+
