d338af1e1fbd390b1392c712fbc7345f0afd4605
dave <michael828inoc@gmail.com>
2023-03-07 00:25:20 +0800

    1:1, 1:M, M:1, M:M

--- db.sql
+++ db.sql
@@ -19,10 +19,65 @@ SELECT id, make, model, price AS original_price,
 
 SELECT COALESCE(email, 'email not provided') FROM person;
 
+CREATE TABLE person (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE car (
+    id SERIAL PRIMARY KEY,
+    person_id INTEGER NOT NULL,
+    plate_number VARCHAR(255) NOT NULL,
+
+    FOREIGN KEY (person_id) REFERENCES person (id)
+);
+
+CREATE TABLE carcolor (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE carcolorrelation (
+    car_id INTEGER NOT NULL,
+    carcolor_id INTEGER NOT NULL,
+
+    FOREIGN KEY (carcolor_id) REFERENCES carcolor(id),
+    FOREIGN KEY (car_id) REFERENCES car(id)
+);
+
 -- INNER JOINS gives rows with car.id
-SELECT * FROM person
-    JOIN car ON person.car_id = car.id;
+SELECT * FROM person JOIN car ON person.car_id = car.id;
 
 -- LEFT JOINS gives all rows despit of car.id being null
-SELECT * FROM person
-    LEFT JOIN car ON car.id = person.car_id; 
+SELECT * FROM person LEFT JOIN car ON car.id = person.car_id; 
+
+insert into person (name) values ('eparfrey0');
+insert into person (name) values ('sreicharz1');
+insert into person (name) values ('rhague2');
+insert into person (name) values ('tswendell3');
+insert into person (name) values ('tgrigori4');
+insert into person (name) values ('bvanderkruijs5');
+insert into person (name) values ('lchimenti6');
+insert into person (name) values ('eroskell7');
+insert into person (name) values ('emunro8');
+insert into person (name) values ('crennebach9');
+
+insert into car (plate_number, person_id) values ('123', 1);
+insert into car (plate_number, person_id) values ('NC 123', 1);
+insert into car (plate_number, person_id) values ('AB 123', 1);
+insert into car (plate_number, person_id) values ('AC 123', 1);
+insert into car (plate_number, person_id) values ('AD 1234', 1);
+insert into car (plate_number, person_id) values ('AE 123', 2);
+insert into car (plate_number, person_id) values ('AF 1', 3);
+
+insert into carcolor (name) values ('red');
+insert into carcolor (name) values ('blue');
+insert into carcolor (name) values ('green');
+
+insert into carcolorrelation (car_id, carcolor_id) values (1, 1);
+insert into carcolorrelation (car_id, carcolor_id) values (1, 2);
+insert into carcolorrelation (car_id, carcolor_id) values (2, 1);
+insert into carcolorrelation (car_id, carcolor_id) values (3, 1);
+insert into carcolorrelation (car_id, carcolor_id) values (3, 3);
+insert into carcolorrelation (car_id, carcolor_id) values (4, 1);
+insert into carcolorrelation (car_id, carcolor_id) values (5, 1);
